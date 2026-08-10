9bd9a9ba44ec79fcf82545bc51c8fd4c395a5136
Mahima <mahima436367@gmail.com>
2026-08-10 23:23:31 +0530

    utils code learning

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
 import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
  
 const app = express()
 
+app.use(cors({
+    origin : CORS_ORIGIN,
+    credentials: true
+}))
+app.use(express.json());
+app.use(express.urlencoded({extended : true}))
+app.use(express.static("public"))
+app.use(cookieParser())
 export { app }
---
+++ src/utils/asynchandler.js
@@ -0,0 +1,17 @@
+const asynchandler = (requesthandler)=>{
+    return (req,res,next)=>{
+    Promise.resolve(requesthandler(req,res,next)).catch((err)=>next(err)) }}
+
+    export { asynchandler }
+
+
+
+/*
+const asynchandler = (fn) => async(req,res,next) =>{
+    try{
+    await fn(req,res,next)}
+    catch(err){
+    res.status(err.code|| 505).json({
+    success: false,
+    mesaage = err.message})}}
+    */
