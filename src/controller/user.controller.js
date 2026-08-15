0eae0a6d80331cbc121f07b69731900a0309a0db
Mahima <mahima436367@gmail.com>
2026-08-15 15:27:58 +0530

    logic building

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,11 +1,70 @@
 // firstly async handler ko import krenge
 import { asynchandler } from "../utils/asynchandler.js"
  // ek const bnayenge yaha pr hum user ko bss register kre honge
+ import { ApiError } from "../utils/apierror.js"
+ import { user } from "../models/user_model.js"
+ import  { uploadcloudinary } from "../utils/cloudinary.js"
+ import { ApiResponse } from "../utils/Apiresponse.js"
+ 
 
+ 
  const registerUser = asynchandler(async(req,res)=>{
-  res.status(200).json({
-        message: "ok"
+  const {fullName,email,username,password}=req.body
+  console.log("email",email)
+  // field check kre hai ki koi empty toh ni hai
+    if(
+        [fullName,email,username,password].some((field)=>
+        field?.trim()==="")
+    ){
+        throw new ApiError(400,"all are fields are required")
+    }
+    //check kre hai koi or user toh ni hai
+   const existeduser= user.findOne({
+        $or : [{username},{email}]
     })
+// if existed user hai toh aage proceed ni nahi krna hai error throw kr dena hai
+ if(existeduser){
+    throw new ApiError(409, "user with this email or username already existed")
+ }
+
+const avatarlocalpath= req.files?.avatar[0]?.path
+const coverlocalpath = req.files?.coverImage[0]?.path 
+
+if(!avatarlocalpath){
+    throw new ApiError(400,"avatar is necessary")
+}
+// upload on cloudinary
+
+const avatar = await uploadcloudinary(avatarlocalpath)
+const cover = await uploadcloudinary(coverlocalpath)
  })
 
-export { registerUser }
+if(!avatar){
+throw new ApiError(400 , "avatar file is required")
+}
+ 
+// database m entry krwa raha hai
+const User = await user.create({
+    fullname,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username : username.toLowerCase()
+
+})
+
+const createduser= await user.findById(user._id).select(
+    "-password -referenceToken"
+)
+
+if(!createduser){
+    throw new ApiError(500,"something went wrong while registering a user")
+}
+
+return res.status(201).json(
+    new ApiResponse(200, createduser,"user enter successfully")
+)
+
+
+export { registerUser }  
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,15 +1,25 @@
 // router bnayenge
 
-// import krenge router ko express se 
+// import krenge router ko express se
 // jaise express se app bnare the waise hi router se route bnayenge
 
 import { Router } from "express";
 import { registerUser } from "../controller/user.controller.js";
+import { upload } from "../middleware/multer.middleware.js";
 
-const router = Router()
-router.route("/register").post(registerUser)
+const router = Router();
+router.route("/register").post(
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount:1,
+    },
+  ]),
+  registerUser
+);
 
-
-export default router
-
-
+export default router;
